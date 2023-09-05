0bb85ce9459245017f242755561e84e6a54c1c1f
rafaelrufino <rafaelrufino.52@gmail.com>
2023-09-05 03:27:11 -0300

    chore: api configuration files

---
+++ api/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended", "eslint:recommended"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {
+    "no-console": "off",
+    "class-methods-use-this": "off",
+    camelcase: "off",
+    "no-promise-executor-return": "off",
+    "import/no-extraneous-dependencies": "off",
+    "consistent-return": "off",
+    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+  },
+};
---
+++ api/README.DOCKER.md
@@ -0,0 +1,14 @@
+- abrir o executavel do docker
+  * docker exec -it pg bash
+- LOGAR no postgres
+  * psql
+- lista as base de dados
+  * \dt
+  - acessar o banco de dados
+    * \c nome_do_banco
+  - listar as tabelas
+    * \dt
+  - listar as colunas
+    * \d nome_da_tabela
+  - listar os dados
+    * select * from nome_da_tabela;
