1a167cc80aa09f39bbb8127f9a9e3f730f39ffea
RafaelRufino <rafaelrufino.52@gmail.com>
2023-09-26 10:48:38 -0300

    chore: include main component

---
+++ front/src/App/index.js
@@ -0,0 +1,29 @@
+import { BrowserRouter } from 'react-router-dom';
+
+import { ThemeProvider } from 'styled-components';
+
+import GlobalStyles from '../assets/styles/global';
+
+import defaultTheme from '../assets/styles/themes/default';
+
+import Routes from '../Routes';
+
+import { Header } from '../components/header';
+
+import * as S from './styles';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <GlobalStyles />
+        <S.Container>
+          <Header />
+          <Routes />
+        </S.Container>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ front/src/App/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-self: center;
+  height: 100%;
+  width: 100%;
+  margin: 0 auto;
+  max-width: 500px;
+  padding: 0 16px;
+`;
