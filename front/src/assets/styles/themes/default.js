1bbfab94098037c757e8b5593ee18f002b8b5754
RafaelRufino <rafaelrufino.52@gmail.com>
2023-09-26 10:41:41 -0300

    feat: create theme apllication

--- front/src/assets/styles/themes/default.js
+++ front/src/assets/styles/themes/default.js
@@ -1,3 +1,25 @@
 export default {
-  backgroundColor: '#f6f5fc',
-}
+  colors: {
+    background: '#f6f5fc',
+    textDark: '#222',
+    white: '#fff',
+    danger: {
+      light: '#F97171',
+      main: '#FC5050',
+      dark: '#F63131',
+    },
+
+    gray: {
+      light: '#BCBCBC',
+      main: '#22222',
+      dark: '#333',
+    },
+
+    primary: {
+      lighter: '#E0E3FF',
+      light: '#6674F4',
+      main: '#5061fc',
+      dark: '#3346F0',
+    },
+  },
+};
