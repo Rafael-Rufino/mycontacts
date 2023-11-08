a81f1a8d7ce8371f582a35bed2efa5c67331f813
Rafael Rufino~ <rafaelrufino.52@gmail.com>
2023-11-08 16:42:32 -0300

    feat: add Modal cand delete function

--- front/src/components/contactsList/Components/ContanctInfo/index.js
+++ front/src/components/contactsList/Components/ContanctInfo/index.js
@@ -1,37 +1,74 @@
 import PropTypes from 'prop-types';
 
-import React from 'react';
+import React, { useState } from 'react';
 
 import { Link } from 'react-router-dom';
 
 import { FiEdit, FiInstagram, FiTrash } from 'react-icons/fi';
+import { Modal } from '../../../modal';
 
 import * as S from './styles';
 
 export function ContanctInfo({
-  name, midia, email, phone,
+  id,
+  name,
+  midia,
+  email,
+  phone,
+  nameConfirme,
 }) {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const isCloseModal = () => setIsModalVisible(false);
+
+  const isOpenModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleDeleteContact = async () => {
+    try {
+      await fetch(`http://localhost:3333/contacts/${id}`, {
+        method: 'DELETE',
+      });
+      isCloseModal();
+    } catch (error) {
+      throw new Error(error);
+    }
+  };
+
+  const handleEditContact = async () => { };
   return (
     <S.Card>
       <S.Wrapper>
         <S.UserInfo>
           <strong>{name}</strong>
-          <small>
-            <FiInstagram size={20} />
-            {midia}
-          </small>
+          {midia && (
+            <small>
+              <FiInstagram size={20} />
+              {midia}
+            </small>
+          )}
         </S.UserInfo>
         <S.WrapperDescrition>
           <S.Description>{email}</S.Description>
           <S.Description>{phone}</S.Description>
         </S.WrapperDescrition>
       </S.Wrapper>
-
+      {isModalVisible && (
+        <Modal
+          title={`Tem Certeza que deseja remover o contato "${nameConfirme}"?`}
+          description="Essa ação não poderá ser desfeita!"
+          cancelButton="Cancelar"
+          confirmButton="Deletar"
+          closeModal={isCloseModal}
+          confirmActionDelete={handleDeleteContact}
+          isDanger
+        />
+      )}
       <S.Actions>
-        <S.ButtonEdit as={Link} to="/edit/123">
+        <S.ButtonEdit as={Link} to={`/edit/${id}`} onClick={handleEditContact}>
           <FiEdit size={24} title="Editar" />
         </S.ButtonEdit>
-        <S.ButtonTrash type="button" title="Excluir">
+        <S.ButtonTrash type="button" title="Excluir" onClick={isOpenModal}>
           <FiTrash size={24} />
         </S.ButtonTrash>
       </S.Actions>
@@ -40,8 +77,10 @@ export function ContanctInfo({
 }
 
 ContanctInfo.prototype = {
+  id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   midia: PropTypes.string.isRequired,
   email: PropTypes.string.isRequired,
   phone: PropTypes.string.isRequired,
+  nameConfirmet: PropTypes.string.isRequired,
 };
