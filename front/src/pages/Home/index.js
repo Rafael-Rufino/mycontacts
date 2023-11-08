e53c51738db5be8ec2e440303e06f72bd35e3196
Rafael Rufino~ <rafaelrufino.52@gmail.com>
2023-11-08 16:43:54 -0300

    feat: add API with integration

--- front/src/pages/Home/index.js
+++ front/src/pages/Home/index.js
@@ -1,30 +1,66 @@
-import React, { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
+
+import { ContactsList } from '../../components/contactsList';
+import { Loader } from '../../components/loader';
+import { SearchInput } from '../../components/searchInput';
+
+import delay from '../../utils/delay';
 
 import * as S from './styles';
 
-import { ContactsList } from '../../components/contactsList';
-import { Modal } from '../../components/modal';
-import { SearchInput } from '../../components/searchInput';
-
 export function Home() {
-  const name = 'Rafael Rufino';
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const isCloseModal = () => setIsModalVisible(false);
+  const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  const filteredContacts = useMemo(() => contacts.filter(
+    (contact) => (
+      contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+    ),
+  ), [contacts, searchTerm]);
+
+  useEffect(() => {
+    const fetchContacts = async () => {
+      try {
+        setIsLoading(true);
+        const data = await fetch(`http://localhost:3333/contacts?orderBy=${orderBy}`);
+        await delay(500);
+        const response = await data.json();
+        setContacts(response);
+      } catch (error) {
+        throw new Error('Error', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchContacts();
+  }, [orderBy]);
+
+  const handleToggleOrderBy = async () => {
+    setOrderBy((prevState) => (prevState === 'asc' ? 'desc' : 'asc'));
+  };
+
+  const handleChangeSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
   return (
     <S.Container>
-      {/* <Loader /> */}
-      <SearchInput />
-      {isModalVisible && (
-        <Modal
-          title={`Tem Certeza que deseja remover o contato "${name}"?`}
-          description="Essa ação não poderá ser desfeita!"
-          cancel="Cancelar"
-          confirm="Deletar"
-          closeModal={isCloseModal}
-          isDanger
-        />
-      )}
-      <ContactsList />
+      <Loader isLoading={isLoading} />
+      <SearchInput
+        type="text"
+        placeholder="Pesquisar pelo nome"
+        value={searchTerm}
+        onChange={handleChangeSearch}
+      />
+      <ContactsList
+        key={contacts.id}
+        onToggleOrderBy={handleToggleOrderBy}
+        orderBy={orderBy}
+        contactsData={filteredContacts}
+      />
     </S.Container>
   );
 }
